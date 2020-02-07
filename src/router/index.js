93724ad8a30a6633425efe562b342df500e98671
1006027560 <1006027560@qq.com>
2020-02-07 11:58:10 +0800

    路由整合

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+//导入vue
+import Vue from 'vue'
+//导入路由
+import router from 'vue-router'
+import VueRouter from 'vue-router'
+//注册路由
+Vue.use(VueRouter)
+//创建路由规则
+const routers = []
+//创建对象
+ const router = new VueRouter({
+    routes
+});
+//暴露出去
+export default router
