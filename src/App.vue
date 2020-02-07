c990616564b8134d2c5541fbc2eeb25a2f34f14a
1006027560 <1006027560@qq.com>
2020-02-07 14:16:16 +0800

    使用element-ui

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
-<router-view></router-view>
+<el-button type="success">成功</el-button>
 </template>
 
 <script>
 export default {
   //组件名字
-  name: "app"
+  // name: "app"
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+//导入element-ui
+import ElementUI from 'element-ui';
+//导入element-ui的样式
+import 'element-ui/lib/theme-chalk/index.css';
+//注册element-ui
+Vue.use(ElementUI);
 import router from './router/index.js'
 
 Vue.config.productionTip = false
 
 new Vue({
+
   render: h => h(App),
   router
 }).$mount('#app')
