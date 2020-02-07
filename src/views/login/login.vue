dd35dd058b68237ff57aae6a86f7062a38a0c757
1006027560 <1006027560@qq.com>
2020-02-07 20:29:47 +0800

    登录页模块完成

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,17 +1,51 @@
 <template>
   <div class="login-container">
-    <div class="left-box"></div>
+    <!-- 左边盒子 -->
+    <div class="left-box">
+      <div class="title-box">
+        <img src="../../assets/login-logo.png" class="logo" />
+        <span class="title">黑马面面</span>
+        <span class="line"></span>
+        <span class="sub-title">用户登录</span>
+      </div>
+      <div class="center">
+        <el-input class="user" placeholder="请输入手机号" prefix-icon="el-icon-user" v-model="input1"></el-input>
+        <el-input
+          class="user"
+          placeholder="请输入密码"
+          prefix-icon="el-icon-lock"
+          v-model="input2"
+          show-password
+        ></el-input>
+        <el-input class="user" placeholder="请输入验证码" prefix-icon="el-icon-key" v-model="input3"></el-input>
+        <el-checkbox v-model="checked">
+          已阅读并同意
+          <el-link  type="primary">用户协议</el-link>和
+          <el-link type="primary">隐私条款</el-link>
+        </el-checkbox>
+      </div>
+      <el-button @click="clickMe" class="my-btn" type="primary">登录</el-button>
+      <el-button class="my-btn" type="primary">注册</el-button>
+    </div>
+    <!-- 右边图片 -->
     <img src="../../assets/login_banner_ele.png" alt />
   </div>
 </template>
 
 <script>
 export default {
-  name: "login"
+  name: "login",
+  data() {
+    return {
+      input1: "",
+      input2: "",
+      input3: ""
+    };
+  }
 };
 </script>
 
-<style>
+<style lang="less">
 .login-container {
   height: 100%;
   background: linear-gradient(
@@ -25,10 +59,53 @@ export default {
   align-items: center;
   /* 左右均分 */
   justify-content: space-around;
-}
-.login-container .left-box {
-  width: 478px;
-  height: 550px;
-  background-color: #f5f5f5;
+
+  .left-box {
+    width: 478px;
+    height: 550px;
+    background-color: #f5f5f5;
+    .title-box {
+      display: flex;
+      align-items: center;
+      margin-top: 44px;
+      margin-left: 20%;
+      .title {
+        font-size: 24px;
+        margin-left: 16px;
+        margin-right: 14px;
+      }
+      .line {
+        width: 1px;
+        height: 28px;
+        background-color: #c7c7c7;
+      }
+      .sub-title {
+        font-size: 21px;
+        margin-left: 12px;
+      }
+    }
+  }
+  .center {
+    margin-left: 43px;
+    margin-top: 30px;
+    margin-bottom: 30px;
+    .user {
+      //   margin-top: 30px;
+      width: 394px;
+      height: 45px;
+      margin-bottom: 30px;
+    }
+    
+  }
+  
+  .my-btn {
+    width: 394px;
+    margin-left: 8%;
+    margin-bottom: 30px;
+  }
+  .my-btn + .my-btn {
+    margin: 0;
+    margin-left: 8%;
+  }
 }
 </style>
